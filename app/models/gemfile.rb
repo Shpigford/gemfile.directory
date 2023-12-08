4fd91aa4821da9e7efc2feb816e5d88d00a1becf
Josh Pigford <josh@joshpigford.com>
2023-12-08 17:46:21 -0600

    Make sure name is present

--- app/models/gemfile.rb
+++ app/models/gemfile.rb
@@ -15,21 +15,20 @@ class Gemfile < ApplicationRecord
     self.content.split("\n").each do |line|
       if line.strip.start_with?("gem")
         gem_name = line.strip.split(" ")[1]
+        
+        # only continue if gem_name.strip is not empty
+        unless gem_name.strip.empty?
+          # remove any quotes from the gem name
+          gem_name = gem_name.gsub(/['"]/, '')
 
-        Rails.logger.warn "Enqueuing NAMMEEE for #{gem_name}"
+          # remove any commas from the gem name
+          gem_name = gem_name.gsub(/[,]/, '')
 
-        # remove any quotes from the gem name
-        gem_name = gem_name.gsub(/['"]/, '')
+          app_gem = AppGem.find_or_create_by(name: gem_name)
+          self.app_gems << app_gem
 
-        # remove any commas from the gem name
-        gem_name = gem_name.gsub(/[,]/, '')
-
-        app_gem = AppGem.find_or_create_by(name: gem_name)
-        self.app_gems << app_gem
-
-        Rails.logger.warn "Enqueuing UpdateGemDataJob for #{app_gem.name} - #{app_gem.id}"
-
-        UpdateGemDataJob.perform_async(app_gem.id)
+          UpdateGemDataJob.perform_async(app_gem.id)
+        end
       end
     end
   end
