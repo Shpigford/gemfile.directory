4cd08611c4820dbe4965066bd21d883e52279198
Josh Pigford <josh@joshpigford.com>
2023-12-08 09:54:35 -0600

    Mostly styling but also some helpers

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,25 @@
 module ApplicationHelper
+  def title(page_title)
+    content_for(:title) { page_title }
+  end
+
+  def header_title(page_title)
+    content_for(:header_title) { page_title }
+  end
+
+  def description(page_description)
+    content_for(:description) { page_description }
+  end
+
+  def meta_image(meta_image)
+    content_for(:meta_image) { meta_image }
+  end
+
+  def header_content(&block)
+    content_for(:header_content, &block)
+  end
+
+  def header_content?
+    content_for?(:header_content)
+  end
 end
