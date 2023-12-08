bf816a9bca942d8aea8af884167d4eb3ed59db3d
Josh Pigford <josh@joshpigford.com>
2023-12-08 09:32:55 -0600

    Custom font

--- config/application.rb
+++ config/application.rb
@@ -23,5 +23,7 @@ module GemfileDirectory
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+
+    config.assets.paths << Rails.root.join("app", "assets", "fonts")
   end
 end
--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -11,6 +11,7 @@ module.exports = {
     extend: {
       fontFamily: {
         sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+        mono: ['ml', ...defaultTheme.fontFamily.mono]
       },
     },
   },
