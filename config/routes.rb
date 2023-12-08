5d905dba9b95639c5711016c2e730ecdba319411
Josh Pigford <josh@joshpigford.com>
2023-12-08 11:21:38 -0600

    Initial pass at gemfile listing

--- config/routes.rb
+++ config/routes.rb
@@ -14,5 +14,5 @@ Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
 
   # Defines the root path route ("/")
-  root "pages#home"
+  root "gemfiles#index"
 end
