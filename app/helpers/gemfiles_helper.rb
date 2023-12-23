a83d8290634512f38c46805a7186cdcff74c6234
Facundo Espinosa <facundo.espinosa@fing.edu.uy>
2023-12-23 13:16:32 -0300

    Use right size for user.image and extract submitted by logic
    The user.image is used directly on all cases and just re-sizeing it with css. The default size of the image is 460x460 and the project uses 32x32 and 16x16 so in favor of use the right size for browser performance, the `user_image_url` helper was created  to attach the  `s` query parameter to the URL so we can request the image in the size we want.
    
    The second change is the extraction of the submitted by logic. That logic was spread in three different files, so the best option to avoid copy/paste is to move it to a helper (`gemfile_submitted_by`) and re-use it when it's needed it.

--- app/helpers/gemfiles_helper.rb
+++ app/helpers/gemfiles_helper.rb
@@ -1,2 +1,21 @@
 module GemfilesHelper
+  def gemfile_submitted_by(user)
+    concat("Submitted by ")
+
+    tag.span(class: "flex item-center ml-2 space-x-1") do
+      concat(image_tag user_image_url(user, size: 16), class: "w-4 h-4 rounded-full")
+
+      concat(
+        tag.b do
+          if user.x_username.present?
+            link_to "@#{user.x_username}", "https://x.com/#{user.x_username}", class: 'hover:underline'
+          elsif user.github_username.present?
+            link_to "@#{user.github_username}", "https://github.com/#{user.github_username}", class: 'hover:underline'
+          else
+            user.name
+          end
+        end
+      )
+    end
+  end
 end
---
+++ app/helpers/user_helper.rb
@@ -0,0 +1,11 @@
+module UserHelper
+  def user_image_url(user, size: nil)
+    return user.image unless size
+
+    uri = URI.parse(user.image)
+    new_query_ar = URI.decode_www_form(uri.query || '') << ["s", size]
+    uri.query = URI.encode_www_form(new_query_ar)
+
+    uri.to_s
+  end
+end
