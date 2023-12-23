5878d557944198cb82079b4aa0353d6a79c19b93
Facundo Espinosa <facundo.espinosa@fing.edu.uy>
2023-12-23 13:58:29 -0300

    Lazy load controllers
    Currently controllers are loaded automatically doing `eagerLoadControllersFrom` that loads every controller on every page always. With this change they will be loaded lazy by the use of `lazyLoadControllersFrom`. In favor of load just what the page needs, the css files required by highlight now are loaded only on gemfile#show given is the only page using it.

--- config/importmap.rb
+++ config/importmap.rb
@@ -1,12 +1,16 @@
 # Pin npm packages by running ./bin/importmap
 
+# Preloaded Packages
 pin "application", preload: true
 pin "@hotwired/turbo-rails", to: "turbo.min.js", preload: true
-pin "@hotwired/stimulus", to: "https://ga.jspm.io/npm:@hotwired/stimulus@3.2.2/dist/stimulus.js"
+pin "@hotwired/stimulus", to: "https://ga.jspm.io/npm:@hotwired/stimulus@3.2.2/dist/stimulus.js", preload: true
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
-pin_all_from "app/javascript/controllers", under: "controllers"
+
+# NOT Preloaded Packages
 pin "stimulus-dropdown", to: "https://ga.jspm.io/npm:stimulus-dropdown@2.1.0/dist/stimulus-dropdown.mjs"
 pin "hotkeys-js", to: "https://ga.jspm.io/npm:hotkeys-js@3.12.2/dist/hotkeys.esm.js"
 pin "stimulus-use", to: "https://ga.jspm.io/npm:stimulus-use@0.51.3/dist/index.js"
 pin "stimulus-reveal-controller", to: "https://ga.jspm.io/npm:stimulus-reveal-controller@4.1.0/dist/stimulus-reveal-controller.mjs"
 pin "highlight.js", to: "https://ga.jspm.io/npm:highlight.js@11.9.0/es/index.js"
+
+pin_all_from "app/javascript/controllers", under: "controllers"
