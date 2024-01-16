27bb6ab4f48e715f0356dc33705a6f6ec443078b
Varun Kumar <varun.kumar@yale.edu>
2024-01-16 17:05:20 -0500

    add controller and route

--- config/routes.rb
+++ config/routes.rb
@@ -26,6 +26,7 @@ Rails.application.routes.draw do
 
   get "search", to: "search#index"
 
+  get "my/top-gemfiles", to: "my#top_gemfiles"
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
