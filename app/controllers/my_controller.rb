778e63ae2432350a5995c3929d7f8c2e23e629b7
Varun Kumar <varun.kumar@yale.edu>
2024-01-17 20:27:03 -0500

    favorite gemfiles method

--- app/controllers/my_controller.rb
+++ app/controllers/my_controller.rb
@@ -1,4 +1,6 @@
 class MyController < ApplicationController
   def favorite_gemfiles
+    @favorite_gemfiles = current_user.favorite_gemfiles
+    #current_user.favorites.map(&:favoritable).with_favorites
   end
 end
