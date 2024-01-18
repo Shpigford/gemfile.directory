d4548e533131225924b26a382fb657a811236776
Varun Kumar <varun.kumar@yale.edu>
2024-01-17 20:32:00 -0500

    sign out button styling

--- app/controllers/my_controller.rb
+++ app/controllers/my_controller.rb
@@ -1,6 +1,5 @@
 class MyController < ApplicationController
   def favorite_gemfiles
     @favorite_gemfiles = current_user.favorite_gemfiles
-    #current_user.favorites.map(&:favoritable).with_favorites
   end
 end
